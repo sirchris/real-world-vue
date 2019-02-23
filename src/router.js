0ac71d189616ef186b22fee71d78e9ef5d9c0554
sirchris <florkowski.krzysztof@gmail.com>
2019-02-23 22:35:28 +0100

    Dynamic Routing & History Mode

--- src/router.js
+++ src/router.js
@@ -3,10 +3,12 @@ import Router from "vue-router";
 import EventCreate from "./views/EventCreate";
 import EventList from "./views/EventList";
 import EventShow from "./views/EventShow";
+import NotFound from "./views/NotFound";
 
 Vue.use(Router);
 
 export default new Router({
+  mode: "history",
   routes: [
     {
       path: "/",
@@ -19,9 +21,15 @@ export default new Router({
       component: EventCreate
     },
     {
-      path: "/event",
+      path: "/event/:id",
       name: "event-show",
-      component: EventShow
+      component: EventShow,
+      props: true
+    },
+    {
+      path: "*",
+      name: "not-found",
+      component: NotFound
     }
   ]
 });
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
     <h1>Events Listing</h1>
+
+    <router-link :to="{ name: 'event-show', params: { id: 1 } }"
+      >Show Event #1</router-link
+    >
   </div>
 </template>
