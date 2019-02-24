72b6b80c79045874dfbbc093dd0a52d796c030c9
sirchris <florkowski.krzysztof@gmail.com>
2019-02-24 12:27:45 +0100

    State & Getters

--- src/store.js
+++ src/store.js
@@ -4,7 +4,38 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: {
+      id: "abc123",
+      name: "Adam Jahr"
+    },
+    categories: [
+      "sustainability",
+      "nature",
+      "animal welfare",
+      "housing",
+      "education",
+      "food",
+      "community"
+    ],
+    todos: [
+      { id: 1, text: "Feed the cat", done: false },
+      { id: 2, text: "Wash the car", done: false },
+      { id: 3, text: "Clean the flat", done: true },
+      { id: 4, text: "Go shopping", done: false }
+    ]
+  },
   mutations: {},
-  actions: {}
+  actions: {},
+  getters: {
+    catLength(state) {
+      return state.categories.length;
+    },
+    activeTodos(state) {
+      return state.todos.filter(todo => !todo.done);
+    },
+    getTodoById: state => id => {
+      return state.todos.find(todo => todo.id === id);
+    }
+  }
 });
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,5 +1,32 @@
 <template>
-  <div class="about">
-    <h1>Create Event</h1>
+  <div>
+    <h1>Create an Event, {{ user.name }}</h1>
+
+    <p>This event was created by {{ user.id }}</p>
+
+    <p>There are {{ catLength }} categories</p>
+    <ul>
+      <li v-for="category in categories" :key="category">{{ category }}</li>
+    </ul>
+
+    <p>There are some active todos:</p>
+    <ul>
+      <li v-for="todo in activeTodos" :key="todo.id">{{ todo.text }}</li>
+    </ul>
+
+    <p>{{ getTodoById(2) }}</p>
   </div>
 </template>
+
+<script>
+import { mapState, mapGetters } from "vuex";
+
+export default {
+  computed: {
+    ...mapGetters(["activeTodos", "catLength", "getTodoById"]),
+    ...mapState(["user", "categories"])
+  }
+};
+</script>
+
+<style scoped></style>
