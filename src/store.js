02ad81710ad47068d48233f25fe1ba956d6cace9
sirchris <florkowski.krzysztof@gmail.com>
2019-02-24 13:02:07 +0100

    Mutations & Actions

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import EventService from "@/services/EventService.js";
 
 Vue.use(Vuex);
 
@@ -23,10 +24,21 @@ export default new Vuex.Store({
       { id: 2, text: "Wash the car", done: false },
       { id: 3, text: "Clean the flat", done: true },
       { id: 4, text: "Go shopping", done: false }
-    ]
+    ],
+    events: []
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    ADD_EVENT(state, event) {
+      state.events.push(event);
+    }
+  },
+  actions: {
+    createEvent({ commit }, event) {
+      return EventService.postEvent(event).then(() => {
+        commit("ADD_EVENT", event.data);
+      });
+    }
+  },
   getters: {
     catLength(state) {
       return state.categories.length;
